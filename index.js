4b9bccd618a47709cda8074e5c899354d4abe4dc
rickypoolay <rickypoullet@gmail.com>
2021-09-26 04:31:58 -0400

    Animating Sections 2/4
    Added animations to the hero page and about page with gsap and scrollTrigger.

--- index.js
+++ index.js
@@ -6,35 +6,37 @@ $(function () {
     gsap.registerPlugin(ScrollToPlugin);
     
 
-    //Custom Eases
-    const easeInOut = ("custom", "M0,0 C0.196,0 0.336,0.082 0.507,0.512 0.626,0.812 0.666,1 1,1 ");
-
     //Creating TimeLines for each Section
 
     var homeTL = gsap.timeline();
-    var aboutTL = gsap.timeline();
+    var aboutTL = gsap.timeline({scrollTrigger: {trigger:'#about', start: "top center", end: "center center"}});
+    var projectsTL = gsap.timeline();
 
-    //Animate Landing Page
+    //Animate Pages
 
         //Home
     homeTL.from('.logo', {duration:.5, opacity:0, delay:.25})
         .from('.nav-links li', {duration: .5, x:100, ease:'power1', stagger:.1})
-        .from('.name', {duration:.5, x:-100, opacity:0, ease:'power1'}, "-=.5")
-        .from('.title', {duration:.5, x:100, opacity:0, ease:'power1'}, "-=.25")
+        .from('#hero .name', {duration:.5, x:-100, opacity:0, ease:'power1'}, "-=.5")
+        .from('#hero .title', {duration:.5, x:100, opacity:0, ease:'power1'}, "-=.5")
 
         //About
-    aboutTL.from('.content-about', {scrollTrigger: {trigger:'.content-about', start:"top center"}, duration:.5, x:-100, opacity:0, ease:'power1'})
-        .from('.content-skills', {duration:.5, x:100, opacity:0, ease:'power1'}, "-=.25")
+        aboutTL.from('.content-about, .content-skills' , {duration:.5,transformOrigin:'top center', boxShadow:'none', backgroundColor:'#1E152A', scaleY:0, ease:'power1', stagger:.25})
+        .from('.content-about h2, .content-about p, .content-skills h2, .skills-list' , {duration: .5, y:100, opacity:0, ease:'power1'}, '-=.25')
 
+        //Project
 
-    //Auto Scroll by fetching innerText of clicked nav-link and inputing to gsap Scroll to
-
+    //Auto Scroll from navbar and text color setting
+        
     $('.logo').click(() => {
         $('.logo').css('color', '#1E152A');
         $('.nav-links li a').each(() => {
             $('.nav-links li a').css('color', 'hsla(0, 0%, 100%, 0.75)');
         })
+        gsap.to(window, .5, {ease: 'power1', scrollTo:(`#hero`)})
     })
+    
+        //Auto Scroll by fetching innerText of clicked nav-link and inputing to gsap Scroll to
 
     $('.nav-links li').click((e) => {
         const clicked = e.target.innerText.toLowerCase();
@@ -42,7 +44,7 @@ $(function () {
             return;
         } else {
             //Scroll
-            gsap.to(window, .5, {ease: 'expo', scrollTo:(`#${clicked}`)})
+            gsap.to(window, .5, {ease: 'power1', scrollTo:(`#${clicked}`)})
 
             //Set Each Nav link to reset
             $('.nav-links li a').each(() => {
@@ -55,8 +57,9 @@ $(function () {
         }
     })
 
+    // Scroll Trigger nav link updates
 
-
+    // gsap.to('nav-link')
 
 
 
